4bddd42c14bb6f87020441d7eb16b4f564c6ebb1
George Cavalcanti Jr <georgemcjr@gmail.com>
2020-11-01 15:43:50 -0300

    Creating fragments and ViewPager

--- app/src/main/java/br/com/gj/giphytest/MainActivity.kt
+++ app/src/main/java/br/com/gj/giphytest/MainActivity.kt
@@ -1,11 +1,28 @@
 package br.com.gj.giphytest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.gj.giphytest.features.favorites.FavoritesFragment
+import br.com.gj.giphytest.features.trending.TrendingFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewPager()
+    }
+
+    private fun setupViewPager() {
+        val fragmentList = arrayListOf(
+            TrendingFragment(),
+            FavoritesFragment()
+        )
+
+        val viewPagerAdapter = MainPagerAdapter(fragmentList, supportFragmentManager, lifecycle)
+
+        viewPager.adapter = viewPagerAdapter
     }
 }
---
+++ app/src/main/java/br/com/gj/giphytest/MainPagerAdapter.kt
@@ -0,0 +1,21 @@
+package br.com.gj.giphytest
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainPagerAdapter(
+    private val fragmentList: ArrayList<Fragment>,
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
