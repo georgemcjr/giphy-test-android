95e228299b20d4a3585cdbb5157651c1d164960d
George Cavalcanti Jr <georgemcjr@gmail.com>
2020-11-01 16:04:28 -0300

    Create tab layout and attach it to view pager

--- app/src/main/java/br/com/gj/giphytest/MainActivity.kt
+++ app/src/main/java/br/com/gj/giphytest/MainActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.gj.giphytest.features.favorites.FavoritesFragment
 import br.com.gj.giphytest.features.trending.TrendingFragment
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val tabTitleResList = arrayListOf(R.string.title_trending, R.string.title_favorites)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupViewPager()
+        setupTabLayout()
     }
 
     private fun setupViewPager() {
@@ -23,6 +27,13 @@ class MainActivity : AppCompatActivity() {
 
         val viewPagerAdapter = MainPagerAdapter(fragmentList, supportFragmentManager, lifecycle)
 
-        viewPager.adapter = viewPagerAdapter
+        viewPager_main.adapter = viewPagerAdapter
+    }
+
+    private fun setupTabLayout() {
+        TabLayoutMediator(tabLayout_main, viewPager_main) { tab, position ->
+            val stringRes = tabTitleResList[position]
+            tab.text = getString(stringRes)
+        }.attach()
     }
 }
