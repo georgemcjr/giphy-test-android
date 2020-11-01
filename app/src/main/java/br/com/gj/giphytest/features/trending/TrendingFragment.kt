f074cb986b5e42369f9acd3fb7cf2a65da69a6ea
George Cavalcanti Jr <georgemcjr@gmail.com>
2020-11-01 17:05:54 -0300

    Create layout for trending screen (grid with recyclerView and view holder's layout)

--- app/src/main/java/br/com/gj/giphytest/features/trending/TrendingFragment.kt
+++ app/src/main/java/br/com/gj/giphytest/features/trending/TrendingFragment.kt
@@ -1,11 +1,13 @@
 package br.com.gj.giphytest.features.trending
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import br.com.gj.giphytest.R
+import kotlinx.android.synthetic.main.fragment_trending.*
 
 class TrendingFragment : Fragment() {
 
@@ -13,8 +15,15 @@ class TrendingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trending, container, false)
+        val view = inflater.inflate(R.layout.fragment_trending, container, false)
+
+        setupRecyclerView()
+
+        return view
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView_trending.layoutManager = GridLayoutManager(context, 2)
     }
 
 }
